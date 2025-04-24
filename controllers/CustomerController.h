e864632b01ce8fd31377785ace7abe30efe43e9c
Srikant Gudi <53587620+srikantgudi@users.noreply.github.com>
2025-04-24 16:43:31 +0530

    removed extra code in CustomerController header file

--- controllers/CustomerController.h
+++ controllers/CustomerController.h
@@ -16,21 +16,3 @@ class CustomerController : public drogon::HttpController<CustomerController>
     void home(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
     void orders(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, long custid);
 };
-
-#pragma once
-
-#include <drogon/HttpController.h>
-
-using namespace drogon;
-
-class ProductController : public drogon::HttpController<ProductController>
-{
-public:
-  METHOD_LIST_BEGIN
-  ADD_METHOD_TO(ProductController::home, "/categories", Get);
-  ADD_METHOD_TO(ProductController::products, "/products/{1}", Get);
-  METHOD_LIST_END
-
-  void home(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-  void products(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, long catid);
-};
